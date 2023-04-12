ab1c8e9bee884d74b2019afb64d2282ae26c81b9
anmol choudhary <78426956+Anmol-choudhary-spec@users.noreply.github.com>
2023-04-12 19:01:06 +0530

    User model Updated

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -13,29 +13,29 @@ const createTask = asyncWrapper(async (req, res) => {
 })
 
 const getTask = asyncWrapper(async (req, res, next) => {
-  const User = await user.findOne({_id : req.body.id})
+  const User = await user.findOne({_id : req.params.id})
   if (!User) {
-    return next(createCustomError(`No task with id : ${req.body.id}`, 404))
+    return next(createCustomError(`No user with id : ${req.params.id}`, 404))
   }
 
   res.status(200).json({ User })
 })
 
 const deleteTask = asyncWrapper(async (req, res, next) => {
-  const User = await user.findOneAndDelete({ _id: req.body.id })
+  const User = await user.findOneAndDelete({ _id: req.params.id })
   if (!User) {
-    return next(createCustomError(`No task with id : ${req.body.id}`, 404))
+    return next(createCustomError(`No user with id : ${req.params.id}`, 404))
   }
   res.status(200).json({ User })
 })
 const updateTask = asyncWrapper(async (req, res, next) => {
-  const User = await user.findOneAndUpdate({_id: req.body.id}, req.body, {
+  const User = await user.findOneAndUpdate({_id: req.params.id}, req.body, {
     new: true,
     runValidators: true,
   })
 
   if (!User) {
-    return next(createCustomError(`No task with id : ${req.body.id}`, 404))
+    return next(createCustomError(`No user with id : ${req.params.id}`, 404))
   }
 
   res.status(200).json({ User })
